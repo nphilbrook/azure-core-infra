fe8f13f1cd0570c736e782511089b8b3d82b434d
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-09 13:46:02 -0400

    Re-add DNS zones

--- dns_tls/main.tf
+++ dns_tls/main.tf
@@ -5,9 +5,9 @@
 #     }
 # }
 
-# resource "azurerm_dns_zone" "zone" {
-#   name                = "${var.location}.${var.environment}.azure.${var.username}.sbx.hashidemos.io"
-#   resource_group_name = var.resource_group_name
+resource "azurerm_dns_zone" "zone" {
+  name                = "${var.location}.${var.environment}.azure.${var.username}.sbx.hashidemos.io"
+  resource_group_name = var.resource_group_name
 
-#   tags = var.default_tags
-# }
+  tags = var.default_tags
+}
